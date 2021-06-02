b416902c131fb92da95cbd6c5a3e3be8dec91112
Mohit Sharma <43213964+sharmohit@users.noreply.github.com>
2021-06-02 08:53:11 +0530

    Parking App Android Project - Created empty project

---
+++ app/src/main/java/com/gbc/parkingapp/MainActivity.java
@@ -0,0 +1,20 @@
+// Group: Project Groups 12
+// Name: Mohit Sharma
+// Student ID: 101342267
+// Group Member: Javtesh Singh Bhullar
+// Member ID: 101348129
+
+package com.gbc.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
